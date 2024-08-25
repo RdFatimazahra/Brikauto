00b9625f76b7e2e273faa223b80b128199e9d405
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-08-26 00:18:09 +0100

    Create service and serviceimpl for product class

---
+++ Backend/src/main/java/com/backend/ServiceImpl/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.backend.ServiceImpl;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
