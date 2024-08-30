b9f7f0473d1b2e8128d07b790d2a88778ba76345
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-08-30 12:24:30 +0100

    Create ProductMapper class

--- Backend/src/main/java/com/backend/Mapper/ProductMapper.java
+++ Backend/src/main/java/com/backend/Mapper/ProductMapper.java
@@ -1,4 +1,16 @@
 package com.backend.Mapper;
 
+
+import com.backend.DTO.ProductDto;
+import com.backend.Model.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface ProductMapper {
+    ProductMapper INSTANCE= Mappers.getMapper(ProductMapper.class);
+
+    Product productDtoToProduct(ProductDto productDto);
+    ProductDto productToProductDto(Product product);
+
 }
