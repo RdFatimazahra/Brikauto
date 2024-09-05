f1aa8f060b3221bff3bfd1b8ee0969cc3c907921
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-09-05 15:27:08 +0100

    Create authenticationRequest class

--- Backend/src/main/java/com/backend/auth/AuthenticationController.java
+++ Backend/src/main/java/com/backend/auth/AuthenticationController.java
@@ -1,16 +1,5 @@
 package com.backend.auth;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class AuthenticationController {
-    private String email;
-    private String password;
+
 }
