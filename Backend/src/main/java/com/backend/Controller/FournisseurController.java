4fd94d5c79432e3eaef5670ccdcc42fb3f5b4134
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-09-07 22:53:12 +0100

    Edit on path RequestMapping in Controller for class Fournisseur

--- Backend/src/main/java/com/backend/Controller/FournisseurController.java
+++ Backend/src/main/java/com/backend/Controller/FournisseurController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Admin/fournisseurs")
+@RequestMapping("/api/v1/auth/Admin/fournisseurs")
+@CrossOrigin(origins = "http://localhost:4200/")
+
 public class FournisseurController {
 
     @Autowired
