572a86c34b8dc1078ca4a065cf0a371d557c7faf
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-09-07 22:46:30 +0100

    Add Controller for class Fournisseur

---
+++ Backend/src/main/java/com/backend/Controller/FournisseurController.java
@@ -0,0 +1,44 @@
+package com.backend.Controller;
+
+import com.backend.DTO.FournisseurDto;
+import com.backend.Service.FournisseurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Admin/fournisseurs")
+public class FournisseurController {
+
+    @Autowired
+    private FournisseurService fournisseurService;
+
+
+    @PostMapping
+    public FournisseurDto createFournisseur(@RequestBody FournisseurDto fournisseurDto) {
+        return fournisseurService.createFournisseur(fournisseurDto);
+    }
+
+
+    @GetMapping("/{id}")
+    public FournisseurDto getFournisseurById(@PathVariable int id) {
+        return fournisseurService.getFournisseurById(id);
+    }
+
+
+    @GetMapping
+    public List<FournisseurDto> getAllFournisseurs() {
+        return fournisseurService.getAllFournisseurs();
+    }
+
+    @PutMapping("/{id}")
+    public FournisseurDto updateFournisseur(@PathVariable int id, @RequestBody FournisseurDto fournisseurDto) {
+        return fournisseurService.updateFournisseur(id, fournisseurDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFournisseur(@PathVariable int id) {
+        fournisseurService.deleteFournisseur(id);
+    }
+}
