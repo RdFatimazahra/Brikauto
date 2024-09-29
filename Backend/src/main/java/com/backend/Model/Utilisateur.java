202d01f73a6f44e073c9f3786d405baec6056a04
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-09-30 00:03:13 +0100

    add jointure btw order, panier and client

--- Backend/src/main/java/com/backend/Model/Utilisateur.java
+++ Backend/src/main/java/com/backend/Model/Utilisateur.java
@@ -1,9 +1,20 @@
 package com.backend.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
 public class Utilisateur extends Personne {
+
+
+    @OneToMany(mappedBy = "utilisateur")
+    private List<Order> commandes;
+
+    @OneToOne(mappedBy = "utilisateur")
+    private Panier panier;
 }
